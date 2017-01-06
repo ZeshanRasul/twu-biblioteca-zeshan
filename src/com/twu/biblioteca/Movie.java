5fd3af39f62fbc3c9479d312609167805ee22948
ZeshanRasul <zeshanrasul90@gmail.com>
2017-01-06 19:46:00 +0000

    Added movie class with tests

---
+++ src/com/twu/biblioteca/Movie.java
@@ -0,0 +1,50 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by zeshanrasul on 06/01/2017.
+ */
+public class Movie {
+
+    private String title;
+    private int year;
+    private String director;
+    private int rating;
+    public boolean checkedOut;
+
+    public Movie(String t, int y, String d, int r) {
+        title = t;
+        year = y;
+        director = d;
+        rating = r;
+        checkedOut = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+
+    public void checkOut() {
+        checkedOut = true;
+    }
+
+    public void returnMovie() {
+        checkedOut = false;
+    }
+
+    public void printDetails() {
+        System.out.printf("\n" + "%-25.30s  %-25.30s  %-25.30s  %-1.30s", getTitle(), getYear(), getDirector(), getRating());
+    }
+}
---
+++ test/com/twu/biblioteca/MovieTest.java
@@ -0,0 +1,62 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by zeshanrasul on 06/01/2017.
+ */
+public class MovieTest {
+    private ByteArrayOutputStream outputStream;
+    private Movie movie;
+
+    @Before
+    public void beforeEach() {
+        movie = new Movie("The Godfather", 1972, "Francis Ford Coppola", 9);}
+
+    @Test
+    public void getTitleShouldReturnTitle() {
+        assertEquals("The Godfather", movie.getTitle());
+    }
+
+    @Test
+    public void getYearShouldReturnYear() {
+        assertEquals(1972, movie.getYear());
+    }
+
+    @Test
+    public void getDirectorShouldReturnDirector() {
+        assertEquals("Francis Ford Coppola", movie.getDirector());
+    }
+
+    @Test
+    public void getRatingShouldReturnRating() {
+        assertEquals(9, movie.getRating());
+    }
+
+    @Test
+    public void checkOutMethodShouldSetCheckedOutValueToTrue() {
+        movie.checkOut();
+        assertEquals(true, movie.checkedOut);
+    }
+
+    @Test
+    public void returnBookMethodShouldSetCheckedOutValueToFalse() {
+        movie.returnMovie();
+        assertEquals(false, movie.checkedOut);
+    }
+
+    @Test
+    public void printDetailsShouldPrintMovieDetailsInColumns() {
+        outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+        movie.printDetails();
+        assertEquals("\nThe Godfather              1972                       Francis Ford Coppola       9", outputStream.toString());
+    }
+}
